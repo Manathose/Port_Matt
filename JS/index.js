8750f0a1618ec8af7633e3c56d5fb6ab6309b368
Manathose <134565923+Manathose@users.noreply.github.com>
2024-05-06 18:32:54 -0600

    layout updates

--- JS/index.js
+++ JS/index.js
@@ -1,12 +1,13 @@
-console.log("test");
 
-$(document).ready(function(){
-$(".dev-menu").hover(function(){
-    $(".dev-menu").css(
-        "background-color", "blue");
-}, function(){
-    $(".dev-menu").css
-        ("background-color", "transparent");
+//Button hover state below. 
+
+//$(document).ready(function(){
     
-});
-    });
+//$(".dev-menu").hover(function(){
+  //  $(".dev-menu").css(
+    //    "background-color", "blue");
+//}, function(){
+  //  $(".dev-menu").css
+  //      ("background-color", "transparent");
+//});
+  //  });
